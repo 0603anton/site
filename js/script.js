a360317515e32dc777f674aa1cd98150984a90ce
0603anton <zorgzb@gmail.com>
2022-12-04 22:10:51 +0200

    04/12

--- js/script.js
+++ js/script.js
@@ -37,4 +37,13 @@ $(document).ready(function(){
         }
 
     );
+    $('.fade').slick({
+        autoplay:true,
+        arrows:false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
 });
