a0e4c4113505f78a60abe7193f43147d244b0372
0603anton <zorgzb@gmail.com>
2022-12-05 13:20:47 +0200

    05/12

--- js/script.js
+++ js/script.js
@@ -30,20 +30,27 @@ $(document).ready(function(){
                         slidesToScroll: 1
                     }
                 }
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
+
             ]
         }
 
     );
-    $('.fade').slick({
-        autoplay:true,
-        arrows:false,
-        dots: true,
-        infinite: true,
-        speed: 500,
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
         fade: true,
-        cssEase: 'linear'
+        asNavFor: '.slider-nav'
     });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+    });
+
 });
+
